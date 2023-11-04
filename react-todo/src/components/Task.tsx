2b367efb45af2f2add4a3887a5f9aa8c78dc0612
deluxesande <deluxesande@gmail.com>
2023-11-05 00:08:48 +0300

    fix

--- react-todo/src/components/Task.tsx
+++ react-todo/src/components/Task.tsx
@@ -37,7 +37,7 @@ const Task = ({ task, setEditTask, setFetching }: Props) => {
         });
         setFetching(true);
 
-        console.log("Task deleted");
+        // console.log("Task deleted");
     };
 
     return (
